8258d478bbfa5db07204af9aaca4c5448ba63d40
Joshua Madrigal <madrigaljosh10@yahoo.com>
2018-10-18 14:27:10 -0500

    made variables for max and min heights, as well as dispersal. Then created 3 functions to generate random colors, size, and vector locations. linked the button to add view controller

--- CUBES/ViewController.swift
+++ CUBES/ViewController.swift
@@ -14,62 +14,48 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        // Set the view's delegate
-        sceneView.delegate = self
-        
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
-        
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
+    var minHeight : CGFloat = 0.2
+    var maxHeight : CGFloat = 0.6
+    var minDispersal : CGFloat = -4
+    var maxDispersal : CGFloat = 4
+    
+    func generateRandomVector() -> SCNVector3 {
+        return SCNVector3(CGFloat.random(in: minDispersal ... maxDispersal), CGFloat.random(in: minDispersal ... maxDispersal), CGFloat.random(in: minDispersal ... maxDispersal))
+    
     }
     
+    func randomColorGenerator() -> UIColor {
+        return UIColor(red: CGFloat.random(in: 0 ... 1), green: CGFloat.random(in: 0 ... 1), blue: CGFloat.random(in: 0 ... 1), alpha: CGFloat.random(in: 0.5 ... 1))
+    }
+    
+    func randomSizeGenerator() -> CGFloat {
+        return CGFloat.random(in: minHeight ... maxHeight)
+        
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        sceneView.delegate = self
+        sceneView.showsStatistics = true
+        
+    }
+    
+    @IBAction func addCubeClicked(_ sender: Any) {
+        
+    }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
-        // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
-
-        // Run the view's session
         sceneView.session.run(configuration)
+        
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        
-        // Pause the view's session
         sceneView.session.pause()
+        
     }
 
-    // MARK: - ARSCNViewDelegate
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
-    }
-}
+} // ViewController Class
